d5975294fcc0da59e77b99bb86b09327bf7782ff
mikaelznx <mikaelpereiradev@gmail.com>
2023-04-06 19:01:26 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+        /*int a = 38;
+        int b =3;
+        System.out.println(a/b);*/
+    }
+}
